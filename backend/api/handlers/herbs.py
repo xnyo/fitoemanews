94bc3183e98aa77ff3cca6818c8ff04ef3bf4a90
Giuseppe Guerra <to@nyo.zz.mu>
2018-06-13 13:03:51 +0200

    Docs api/handlers/herbs.py

--- backend/api/handlers/herbs.py
+++ backend/api/handlers/herbs.py
@@ -31,21 +31,32 @@ from singletons.emanews import EmaNews
 async def handle(request: Request, *, session: Session, params):
     herbs = []
 
+    # Condizioni WHERE
     filters: List[str] = []
     if params["query"]:
+        # WHERE per query ricerca
         params["query"] = "%{}%".format(params["query"])
         filters.append("({})".format(
             " OR ".join(["{} LIKE %(query)s".format(x) for x in ('latin_name', 'botanic_name', 'english_name')])
         ))
 
+    # Join AND delle condizioni del WHERE
     filters_string = " AND ".join(filters)
+
+    # Clausola LIMIT
     if not params["limit"]:
+        # No LIMIT
         limit = ""
     elif params["limit"] and not params["page"]:
+        # LIMIT senza pagina
         limit = "LIMIT {}".format(params["limit"])
     else:
+        # LIMIT con pagina
         limit = "LIMIT {}, {}".format(params["page"] * params["limit"], params["limit"])
 
+    # Costruisci due query identiche (results_query e count_query)
+    # per prendere i risultati che rispecchiano gli argomenti forniti
+    # e i risultati totali (query necessaria poichè è possibile implementare la clausola LIMIT)
     results_query, count_query = [
         "SELECT {what} FROM herbs {where} {order_by} ".format(
             what=x,
@@ -53,34 +64,59 @@ async def handle(request: Request, *, session: Session, params):
             order_by="ORDER BY {} {}".format(params["order_by"], params["direction"])
         ) for x in ("*", "COUNT(*) AS count")
     ]
+
+    # Aggiunti il LIMIT nella query dei risultati DOPO la comprehension
+    # in questo modo count_query non ha la clausola LIMIT
     results_query += limit
+
+    # Prima connessione con SSDictCursor (per prendere X righe alla volta e non tutte le righe insieme,
+    # in questo modo si riduce notevolmente l'uso di memoria durante l'esecuzione della query quando
+    # ci sono query che ritornano molte righe)
     async with EmaNews().db.acquire() as conn:
         async with conn.cursor(SSDictCursor) as cur:
             logging.debug(results_query)
             logging.debug(params)
             await cur.execute(results_query, {**params})
+
+            # Recupera tutti i chunk
             while True:
                 herbs_chunk = await cur.fetchmany()
                 if not herbs_chunk:
                     break
 
+                # Per ogni erba nel chunk...
                 for herb in herbs_chunk:
+                    # ...recupera i documenti se richiesto
                     if params["fetch_documents"]:
+                        # Nuova connessione (DictCursor classico, poichè i documenti per ogni erba sono pochi)
                         async with EmaNews().db.acquire() as doc_conn:
                             async with doc_conn.cursor() as doc_cur:
                                 await doc_cur.execute("SELECT * FROM documents WHERE herb_id = %s", (herb["id"],))
                                 herb["documents"] = await doc_cur.fetchall()
+
+                    # Aggiungi l'erba + documenti alla lista totale delle erbe
                     herbs.append(herb)
 
-    async with EmaNews().db.acquire() as conn:
-        async with conn.cursor() as cur:
-            logging.debug(count_query)
-            await cur.execute(count_query, {**params})
-            total_herbs = await cur.fetchone()
-            if not total_herbs:
-                total_herbs = 0
-            else:
-                total_herbs = total_herbs["count"]
+    # Determina il numero totale di righe
+    if not limit:
+        # Se non c'è la clausola LIMIT, il numero totale di risultati è uguale
+        # alla lunghezza di total_herbs
+        total_herbs = len(herbs)
+    else:
+        # Altrimenti, apri una nuova connessione ed esegui count_query
+        # (uguale alla query per il fetch dei risultati, ma senza clausola
+        # LIMIT e con SELECT COUNT(*) invece che SELECT *
+        async with EmaNews().db.acquire() as conn:
+            async with conn.cursor() as cur:
+                logging.debug(count_query)
+                await cur.execute(count_query, {**params})
+                total_herbs = await cur.fetchone()
+                if not total_herbs:
+                    total_herbs = 0
+                else:
+                    total_herbs = total_herbs["count"]
+
+    # Ritorna il risultato al client HTTP
     return web.json_response({
         "herbs": herbs,
         "total": total_herbs
