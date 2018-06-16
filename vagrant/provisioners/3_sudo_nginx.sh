19c2cb1c53669e42f5f3506f69e6f334fce752f4
Giuseppe Guerra <to@nyo.zz.mu>
2018-06-16 22:29:06 +0200

    Documentazione API con Swagger

--- vagrant/provisioners/3_sudo_nginx.sh
+++ vagrant/provisioners/3_sudo_nginx.sh
@@ -3,6 +3,7 @@
 echo "=> Configuring nginx"
 rm -rf /etc/nginx/sites-enabled/default
 cp /vagrant/vagrant/configs/fitoemanews.nginx.conf /etc/nginx/conf.d/fitoemanews.conf
+sed -i -e "s/sendfile on/sendfile off/" /etc/nginx/nginx.conf
 sudo nginx -t
 sudo nginx -s reload
 
